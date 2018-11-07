1c9d98d8fba41eb46800ada26d75fc44bcf0a41c
Jacob Merizian <jmerizia@vt.edu>
2018-11-07 07:41:38 +0000

    got sql statements working, and example histogram of cost burdens

--- server/app.js
+++ server/app.js
@@ -2,6 +2,29 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const port = parseInt(fs.readFileSync('port.txt'));
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'thads',
+    password: 'nut'
+});
+
+app.get('/sql', (req, res) => {
+    con.query(req.query.sql, function (err, results, fields) {
+        if (err) console.err(err);
+        // map the data so that we just have an for each column
+        to_send = {}
+        fields.forEach(function (field) {
+            to_send[field.name] = [];
+            results.forEach(function (row) {
+                to_send[field.name].push(row[field.name]);
+            });
+        })
+        res.send(JSON.stringify({result: to_send}));
+    })
+});
 
 app.use('/', express.static('files'));
 
--- server/files/main.js
+++ server/files/main.js
@@ -1,13 +1,24 @@
 var P = Plotly
 plot = document.getElementById('plot')
 
-x = []
-for (var i = 0; i < 500; i ++) {
-        x[i] = Math.random();
+function query(sql, cb) {
+    var result = axios.get('/sql', {
+        params: {sql: sql}
+    }).then(function (results) {
+        cb(results.data.result)
+    })
 }
 
-P.newPlot({
-    x: x,
-    type: 'histogram',
+x = []
+for (var i = 0; i < 500; i ++) {
+    x[i] = Math.random();
+}
 
-})
+query('select BURDEN from raw limit 1000', function (result) {
+    console.log(result);
+    P.newPlot('plot', [{
+        x: result['BURDEN'],
+        type: 'histogram',
+        xbins: {start: 0, size: 0.05, end: 5},
+    }])
+});
