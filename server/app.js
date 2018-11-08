acc1ee6c45c92cc8c4fb81691bd97c319a6d302a
Jacob Merizian <jmerizia@vt.edu>
2018-11-08 01:12:45 +0000

    multi sql statements and performance tests

--- server/app.js
+++ server/app.js
@@ -8,21 +8,34 @@ var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'thads',
-    password: 'nut'
+    password: 'nut',
+    multipleStatements: true,
 });
 
 app.get('/sql', (req, res) => {
     con.query(req.query.sql, function (err, results, fields) {
-        if (err) console.err(err);
-        // map the data so that we just have an for each column
-        to_send = {}
-        fields.forEach(function (field) {
-            to_send[field.name] = [];
-            results.forEach(function (row) {
-                to_send[field.name].push(row[field.name]);
-            });
-        })
-        res.send(JSON.stringify({result: to_send}));
+        if (err) {
+            res.send({'error': err});
+            console.log(err);
+            return;
+        }
+        if (!parseInt(req.query.multiple)) {
+            results = [results];
+            fields = [fields];
+        }
+        // map the data so that we just have one for each column
+        var response = [];
+        results.forEach(function (result, idx) {
+            to_send = {}
+            fields[idx].forEach(function (field) {
+                to_send[field.name] = [];
+                result.forEach(function (row) {
+                    to_send[field.name].push(row[field.name]);
+                });
+            })
+            response.push(to_send);
+        });
+        res.send(JSON.stringify({result: response}));
     })
 });
 
--- server/files/main.js
+++ server/files/main.js
@@ -2,23 +2,65 @@ var P = Plotly
 plot = document.getElementById('plot')
 
 function query(sql, cb) {
+    var multiple = 0;
+    if (sql.indexOf(';') > -1) multiple = 1;
     var result = axios.get('/sql', {
-        params: {sql: sql}
+        params: {sql: sql, multiple: multiple}
     }).then(function (results) {
-        cb(results.data.result)
+        if (results.data.error) {
+            console.error(results.data.error);
+        } else {
+            cb(results.data.result);
+        }
     })
 }
 
-x = []
-for (var i = 0; i < 500; i ++) {
-    x[i] = Math.random();
-}
-
-query('select BURDEN from raw limit 1000', function (result) {
-    console.log(result);
-    P.newPlot('plot', [{
-        x: result['BURDEN'],
+// pro tip: don't use semicolons if you only have one sql statement!
+// (it fucks it up)
+query('select BURDEN from raw limit 100000', function (result) {
+    query = result[0];
+    P.newPlot('big_plot', [{
+        x: query['BURDEN'],
         type: 'histogram',
         xbins: {start: 0, size: 0.05, end: 5},
     }])
+});
+
+
+var q = 'select BURDEN from raw where REGION = 1;' +
+        'select BURDEN from raw where REGION = 2;' +
+        'select BURDEN from raw where REGION = 3;' +
+        'select BURDEN from raw where REGION = 4';
+
+var xbins = {start: 0, size: 0.05, end: 5};
+ 
+query(q, function (result) {
+    // note that result is an array for each query!
+    query1 = result[0];
+    query2 = result[1];
+    query3 = result[2];
+    query4 = result[3];
+    P.newPlot('plot1', [{
+        x: query1['BURDEN'],
+        type: 'histogram',
+        xbins: xbins,
+    }], {title: 'Region 1'})
+
+    P.newPlot('plot2', [{
+        x: query2['BURDEN'],
+        type: 'histogram',
+        xbins: xbins,
+    }], {title: 'Region: 2'})
+
+    P.newPlot('plot3', [{
+        x: query3['BURDEN'],
+        type: 'histogram',
+        xbins: xbins,
+    }], {title: 'Region: 3'})
+
+    P.newPlot('plot4', [{
+        x: query4['BURDEN'],
+        type: 'histogram',
+        xbins: xbins,
+    }], {title: 'Region: 4'})
 });
